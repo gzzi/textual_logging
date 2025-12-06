1f6bba6c36aaa23207879dc5d04abb1b6393e5d3
Guillaume Galeazzi <24816508+gzzi@users.noreply.github.com>
2025-12-06 14:16:30 +0100

    feat: use textual

--- log.py
+++ log.py
@@ -1,66 +1,58 @@
-
 import logging
+from textual.app import App
+from textual.widgets import Log
+from textual.logging import TextualHandler
 from time import sleep
-import threading
-import sys,tty,os,termios
-import termios, fcntl, sys, os
-
-stop_threads = False
-
-def handle_key(c):
-    global stop_threads
-    if c == 'q':
-        stop_threads = True
-    elif c == 'h':
-        print("Help: Press 'q' to quit.")
-    if c == 't':
-        log = logging.getLogger()
-        fmt = logging.Formatter(
-'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        log.handlers[0].setFormatter(fmt)
-
-def ctrl_thread():
-    global stop_threads
-    while not stop_threads:
-
-        fd = sys.stdin.fileno()
-
-        oldterm = termios.tcgetattr(fd)
-        newattr = termios.tcgetattr(fd)
-        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-        termios.tcsetattr(fd, termios.TCSANOW, newattr)
-
-        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
-
-        try:
-            while not stop_threads:
-                try:
-                    c = sys.stdin.read(1)
-                    if c:
-                        handle_key(c)
-                except IOError: pass
-
-                sleep(0.1)
-
-        finally:
-            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-            fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+from textual import work
+from textual._context import active_app
+import sys
+from threading import get_ident
 
 
-def log_thread():
-    log = logging.getLogger('toto')
-    while not stop_threads:
-        log.info('toto')
-        sleep(0.25)
+
+class LogApp(App):
+    """An app with a simple log."""
+    app = None
+
+    def __init__(self):
+        self.tid = get_ident()
+        LogApp.app = self
+        super().__init__()
+
+    def compose(self):
+        yield Log()
+
+    def on_ready(self) -> None:
+        log = self.query_one(Log)
+        log.write_line("Hello, World!")
+        self.do_some_log()
+
+    @work(thread=True)
+    def do_some_log(self) -> None:
+        for i in range(3):
+            logging.warning(f"Logging line {i} from thread {get_ident()}")
+            sleep(0.25)
+
+class LogAppHandler(logging.Handler):
+    """A Logging handler for Textual apps."""
+
+    def emit(self, record: logging.LogRecord) -> None:
+        """Invoked by logging."""
+        message = self.format(record)
+        if LogApp.app is None:
+            print(message, file=sys.stderr)
+        else:
+            log = LogApp.app.query_one(Log)
+            if LogApp.app.tid != get_ident():
+                LogApp.app.call_from_thread(log.write_line, message)
+                # app.call_from_thread(log.refresh_lines, 3, 3)
+            else:
+                log.write_line(message)
+                # log.refresh_lines(3, 3)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    ctrl_thread = threading.Thread(target=ctrl_thread)
-    try:
-        ctrl_thread.start()
-        log_thread()
-    except (KeyboardInterrupt, SystemExit):
-        stop_threads = True
-        os.system('stty sane')
-    ctrl_thread.join()
+    logging.basicConfig(
+    level=logging.INFO,
+    handlers=[LogAppHandler()],
+    )
+    LogApp().run()
