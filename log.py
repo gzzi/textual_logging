ab53ce60f44ee0703aa6ee3f0518dc320197f210
Guillaume Galeazzi <24816508+gzzi@users.noreply.github.com>
2025-12-06 14:58:11 +0100

    feat: buffered

--- log.py
+++ log.py
@@ -1,58 +1,105 @@
 import logging
 from textual.app import App
-from textual.widgets import Log
-from textual.logging import TextualHandler
-from time import sleep
+from textual.widgets import Log, Header, Footer
 from textual import work
-from textual._context import active_app
-import sys
 from threading import get_ident
 
+
+class LoggingLogWidget(Log):
+    """A Log widget that captures logging output."""
+
+
+    class LogAppHandler(logging.Handler):
+        """A Logging handler for Textual apps."""
+
+        def __init__(self, app, log_widget: 'LoggingLogWidget') -> None:
+            self.app = app
+            self.log_widget = log_widget
+            self.tid = get_ident()
+            self.lines: list[str] = []
+            super().__init__()
+
+        def emit(self, record: logging.LogRecord) -> None:
+            """Invoked by logging."""
+            self.lines.append(self.format(record))
+
+        def flush(self) -> None:
+            """Flush any remaining log lines."""
+            if self.lines:
+                if self.tid != get_ident():
+                    self.app.call_from_thread(self.log_widget.write_lines, self.lines)
+                else:
+                    self.log_widget.write_lines(self.lines)
+                self.lines.clear()
+
+
+    def __init__(self, logger=None, *args, **kwargs):
+        self.logger = logging.getLogger(logger)
+        super().__init__(*args, **kwargs)
+
+    def on_mount(self) -> None:
+        """Called when the widget is mounted."""
+        self.handler = LoggingLogWidget.LogAppHandler(self.app, self )
+        self.backup_handler = []
+        for h in self.logger.handlers:
+            self.backup_handler.append(h)
+            self.logger.removeHandler(h)
+        self.logger.addHandler(self.handler)
+        self.set_interval(1/25, self.handler.flush)
+
+    def on_unmount(self) -> None:
+        """Called when the widget is unmounted."""
+        self.handler.flush()
+        handlers = self.logger.handlers[:]
+        for handler in handlers:
+            if isinstance(handler, LoggingLogWidget.LogAppHandler):
+                self.logger.removeHandler(handler)
+        for handler in self.backup_handler:
+            self.logger.addHandler(handler)
 
 
 class LogApp(App):
     """An app with a simple log."""
     app = None
 
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),
+                ("c", "clear", "Clear")]
+
     def __init__(self):
         self.tid = get_ident()
         LogApp.app = self
         super().__init__()
 
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+    def action_clear(self) -> None:
+        """An action to clear the log."""
+        log = self.query_one(Log)
+        log.clear()
+
     def compose(self):
-        yield Log()
+        yield Header()
+        yield LoggingLogWidget()
+        yield Footer()
 
     def on_ready(self) -> None:
-        log = self.query_one(Log)
-        log.write_line("Hello, World!")
         self.do_some_log()
 
     @work(thread=True)
     def do_some_log(self) -> None:
-        for i in range(3):
+        for i in range(10000):
             logging.warning(f"Logging line {i} from thread {get_ident()}")
-            sleep(0.25)
 
-class LogAppHandler(logging.Handler):
-    """A Logging handler for Textual apps."""
-
-    def emit(self, record: logging.LogRecord) -> None:
-        """Invoked by logging."""
-        message = self.format(record)
-        if LogApp.app is None:
-            print(message, file=sys.stderr)
-        else:
-            log = LogApp.app.query_one(Log)
-            if LogApp.app.tid != get_ident():
-                LogApp.app.call_from_thread(log.write_line, message)
-                # app.call_from_thread(log.refresh_lines, 3, 3)
-            else:
-                log.write_line(message)
-                # log.refresh_lines(3, 3)
 
 if __name__ == "__main__":
     logging.basicConfig(
     level=logging.INFO,
-    handlers=[LogAppHandler()],
+    #  handlers=[],
     )
+    logging.error("Starting LogApp")
     LogApp().run()
+    logging.error("END LogApp")
