8d9147e47823ce6b6f375dffed0cc70ac110fd9c
Guillaume Galeazzi <24816508+gzzi@users.noreply.github.com>
2025-12-18 17:40:54 +0100

    feat: allow custom format class

--- src/textual_logging/formatter.py
+++ src/textual_logging/formatter.py
@@ -10,20 +10,24 @@ class FormatPart:
     active: bool = True
 
 
-class DynamicFormatter:
+class DynamicFormatter(Formatter):
     """A logging formatter that can change its format dynamically."""
 
-    def __init__(self, fmt: list[FormatPart]):
-        self._fmt = fmt
+    def __init__(
+        self, fmt: list[FormatPart], formatter_cls: type[Formatter] = Formatter
+    ) -> None:
+        super().__init__()
+        self.parts = fmt
+        self.formatter_cls = formatter_cls
         self.refresh_format()
 
     def get_parts(self) -> list[FormatPart]:
         """Get the format parts."""
-        return self._fmt
+        return self.parts
 
     def toggle_part(self, key: str) -> None:
         """Toggle a part of the format on or off."""
-        for part in self._fmt:
+        for part in self.parts:
             if part.key == key:
                 part.active = not part.active
                 break
@@ -31,8 +35,8 @@ class DynamicFormatter:
 
     def refresh_format(self) -> None:
         """Set a new format for the formatter."""
-        fmt = " ".join(part.fmt for part in self._fmt if part.active)
-        self._formatter = Formatter(fmt)
+        fmt = " ".join(part.fmt for part in self.parts if part.active)
+        self._formatter = self.formatter_cls(fmt)
 
     def format(self, record):
         """Format a log record."""
